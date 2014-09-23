93270911b387191ab85423805eb52f6654067348
bbeltramo <bbeltramo@14521-ZD-MAC.local>
2014-09-23 09:23:13 -0700

    Learning Bootstrap
    Using meetings reg page to play with Bootstrap.

--- com_event_manager/site/views/meeting/tmpl/default.php
+++ com_event_manager/site/views/meeting/tmpl/default.php
@@ -13,4 +13,33 @@ JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
 
 ?>
-Coming Soon
+<div class="event-manager-meeting">
+	<div class="page-header">
+		<h1>Meeting Request</h1>
+	</div>
+	<p>Hosting meetings is one of the services offered to OVW TA providers through the TA2TA project. TA providers should complete the following form and someone will respond to the request within two business days.</p>
+	<form role="form" id="registrationForm">
+<div id="reg-step1" class="reg-step-wrapper">
+
+ <meta charset="utf-8">
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+  
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+
+</div>
+
+	</form>

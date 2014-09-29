4016cbd564973712c245345ec8d401f6cc5e5c71
Zachary <zachary@zacharydraper.com>
2014-09-29 16:54:58 -0700

    Added four new positions to the template
    Added manual-1 through manual-4. These do not appear in the template,
    but are instead used to manually load modules into articles using the
    {loadposition xx} feature.

---
+++ mod_log_in_button/mod_log_in_button.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* @package  mod_log_in_button
+* @copyright (C) 2014 NCFJCJ. All rights reserved.
+*/
+
+// no direct access
+defined('_JEXEC') or die;
+
+// Determines wether or not the user is logged in.
+$logged_in = false;
+	
+// get the user groups for this user
+$user_groups = JFactory::getUser()->getAuthorisedGroups();	
+
+// check if the user is a member of 'guests'
+if(!in_array(9, $user_groups)){
+	$logged_in = true;
+}
+
+
+
+?>
+<?php if($logged_in): ?>
+<a class="btn <?php echo $params->get('btn_class', 'btn-primary btn-lg'); ?>" href="<?php echo JRoute::_('index.php?Itemid='.$params->get('menu_item')); ?>">
+	<?php 
+	$btn_icon = $params->get('btn_icon', '');
+	if(!empty($btn_icon)):
+	?>
+	<span class="<?php echo $btn_icon; ?>"></span>&nbsp;
+	<?php endif; ?>
+	<?php echo $params->get('btn_text'); ?>
+</a>
+<?php else: ?>
+<a class="btn <?php echo $params->get('nli_btn_class', 'btn-default btn-lg'); ?>" href="<?php echo JRoute::_('index.php?Itemid='.$params->get('nli_menu_item')); ?>">
+	<?php 
+	$btn_icon = $params->get('nli_btn_icon', '');
+	if(!empty($btn_icon)):
+	?>
+	<span class="<?php echo $btn_icon; ?>"></span>&nbsp;
+	<?php endif; ?>
+	<?php echo $params->get('nli_btn_text', 'Sign In'); ?>
+</a>
+<?php endif; ?>
